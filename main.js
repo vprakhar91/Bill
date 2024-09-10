952385787ab25917c274599b3a987d699f43b404
Prakhar Verma <46259033+vprakhar91@users.noreply.github.com>
2024-09-10 10:29:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+
+const billDivide = document.getElementById('bill-divider');
+
+billDivide.addEventListener('submit', billDividerHandler);
+
+function billDividerHandler(event){
+    event.preventDefault();
+    const inputs = getInputs();
+    showCalculatedOutput(inputs);
+}
+
+
+function getInputs(){
+    let subtotal = document.getElementById('subtotal').value;
+    let tipPercent = document.getElementById('tip').value;
+    let noOfPerson = document.getElementById('no-of-person').value;
+    return {subtotal, tipPercent, noOfPerson};
+}
+
+
+function showCalculatedOutput(input){
+    billDivideForm.reset();
+
+    let totalTip = (parseFloat(input.subtotal) * parseFloat(input.tipPercent)) / 100;
+    let totalAmount = parseFloat(input.subtotal) + totalTip;
+    let tipPerPerson = totalTip / parseFloat(input.noOfPerson);
+    let billPerPerson = totalAmount / parseFloat(input.noOfPerson);
+
+
+    document.getElementById('total-bill').innerHTML = `$ ${totalAmount.toFixed(2)}`;
+    document.getElementById('bill-per-person').innerHTML = `$ ${billPerPerson.toFixed(2)}`;
+    document.getElementById('total-tip').innerHTML = `$ ${totalTip.toFixed(2)}`;
+    document.getElementById('tip-per-person').innerHTML = `$ ${tipPerPerson.toFixed(2)}`;
+}
